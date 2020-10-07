4be535890cb7d20e1ff086364cd0fbd5825c3f35
Grant Orchard <go@hashicorp.com>
2020-10-07 14:54:39 +1100

    change auto_accept

--- main.tf
+++ main.tf
@@ -84,7 +84,6 @@ resource "aws_route53_record" "aws_sub_zone_ns" {
 resource "aws_vpc_peering_connection" "this" {
   vpc_id        = module.vpc.vpc_id
   peer_vpc_id   = local.remote_vpc_id
-  auto_accept   = true
 
   tags = var.tags
 }
