61f59ac6a0620c8fdd908e43f2f2abe35675d29a
Grant Orchard <go@hashicorp.com>
2021-05-07 19:09:55 +1000

    update output attributes

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
-output vpc_id {
-  value = module.vpc.vpc_id
-}
+# output vpc_id {
+#   value = module.vpc.vpc_id
+# }
 
-output private_subnets {
-  value = module.vpc.private_subnets
-}
+# output private_subnets {
+#   value = module.vpc.private_subnets
+# }
 
-output public_subnets {
-  value = module.vpc.public_subnets
-}
+# output public_subnets {
+#   value = module.vpc.public_subnets
+# }
 
-output security_group_ssh {
-  value = module.security_group_ssh.security_group_id
-}
+# output security_group_ssh {
+#   value = module.security_group_ssh.security_group_id
+# }
 
-output security_group_outbound {
-  value = module.security_group_outbound.security_group_id
-}
+# output security_group_outbound {
+#   value = module.security_group_outbound.security_group_id
+# }
 
-output account_id {
-  value = data.aws_caller_identity.current.account_id
-}
+# output account_id {
+#   value = data.aws_caller_identity.current.account_id
+# }
